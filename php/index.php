6518161dc375b88fed3227c685c0f6d77c231c04
thiagousa <thiago@thiagodsantos.com>
2018-06-27 10:15:51 -0400

    exemplo 8

--- php/index.php
+++ php/index.php
@@ -1,12 +1,36 @@
 <?php
 
-$names = [
-'Bruno',
-'Thiago',
-'Lucas',
+// title , due , assigned_to , completed
 
+$taks = array (
 
-];
+array(
+'title' => 'Go to the mall',
+'due'=> 'Today',
+'assigned_to' => 'Thiago',
+'completed' => 'Yes',
+),
+array(
+    'title' => 'Go to the grocery',
+    'due'=> 'Tomorow',
+    'assigned_to' => 'Mary',
+    'completed' => 'No',
+),
+array(
+        'title' => 'Send E-mail',
+        'due'=> 'Yesterday',
+        'assigned_to' => 'Joao',
+        'completed' => 'Yes',
+),
+);
 
+foreach($taks as $tak){
 
-require 'index.view.php';
+echo '<dl style="margin-bottom: 1em;">';
+foreach ($tak as $key => $value) {
+    echo "<dt>$key</dt><dd>$value</dd>";
+
+    echo '</dl>';
+}
+}
+//require 'index.view.php';
