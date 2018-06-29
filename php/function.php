98544ff450b4b96f74a1fe0346a406a696e45af4
thiagousa <thiago@thiagodsantos.com>
2018-06-29 08:58:14 -0400

    age MAX club

---
+++ php/function.php
@@ -0,0 +1,16 @@
+<?php 
+
+function ageClub ($age){
+
+if ($age<=15){
+
+    echo 'Go out, KID';
+
+}
+else {
+
+    echo 'Go in Man';
+
+}
+
+}
--- php/index.php
+++ php/index.php
@@ -1,15 +1,9 @@
 <?php
 
-// title , due , assigned_to , completed
 
-$task =[
-
-'title' => 'Go to the mall',
-'due'=> 'Today',
-'assigned_to' => 'Thiago',
-'completed' => true,
-
-];
+require 'function.php';
 
 
-require 'index.view.php';
+$age = 16;
+
+echo ageClub($age);
